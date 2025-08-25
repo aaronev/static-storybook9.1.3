41eb73907c95c7b961641908034cde26916e499f
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-08-25 14:56:18 -0700

    sandbox initialized

---
+++ assets/Header.stories-CHyF---F.js
@@ -0,0 +1,7 @@
+import{H as a}from"./Header-CTdPQBP4.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-Cm55vZEY.js";import"./preload-helper-D9Z9MdNV.js";import"./Button-CKWJcraP.js";/* empty css               */const{fn:r}=__STORYBOOK_MODULE_TEST__,d={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:r(),onLogout:r(),onCreateAccount:r()}},e={args:{user:{name:"Jane Doe"}}},o={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source}}};const g=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,g as __namedExportsOrder,d as default};
